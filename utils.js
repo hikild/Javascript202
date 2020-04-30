2a1edd0ba198b03f65c2a311c644cb53bfb44f23
Ezequiel Bertti <ebertti@gmail.com>
2020-04-29 23:26:55 -0300

    aula 6

---
+++ utils.js
@@ -0,0 +1,15 @@
+
+exports.variavel = 10
+
+
+function qualquer() {
+
+}
+exports.qualquer = qualquer
+
+
+
+function primeiroNome(nomeCompleto){
+  return nomeCompleto.split(' ')[0]
+}
+exports.primeiroNome = primeiroNome
