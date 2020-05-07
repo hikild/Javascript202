5b20e3f013cded45e54453952b6a97d2c9263683
Ezequiel Bertti <ebertti@gmail.com>
2020-05-06 22:00:56 -0300

    correcao tp2

--- tp2.meuNomeAluno.js
+++ tp2.meuNomeAluno.js
@@ -1,26 +1,57 @@
 // Meu Nome
 
 function questao1() {
-  // qtd 2 e 3 entre 0 e 1000000
-
+  let qtd = 0
+  for(let i = 1; i < 1000000; i++){
+    if(i % 2 === 0 && i % 3 === 0){
+      qtd++
+    }
+  }
+  console.log(qtd)
 }
 
 function questao2() {
+  let inicio = Math.floor(new Date().getTime())
   // 53!
 
+  let resultado = 1
+  for(let i = 1; i <= 53; i++){
+    resultado *= i
+  }
+
+  let fim = Math.floor(new Date().getTime())
+  let total = fim - inicio
+  console.log(total, 'em segundos', 'resultado', resultado)
 }
 
 function questao3() {
-  let aluno = [
-    {nr: 1, nota: 10},
-    {nr: 2, nota: 10},
-    {nr: 3, nota: 10},
-    {nr: 4, nota: 10},
-    {nr: 5, nota: 10},
-    /// ate 20
-    {nr: 20, nota: 10}
-  ]
+  let qtd_alunos = 19
+  let aprovados = 0
+  let alunos = []
+  for(let i = 1; i <= qtd_alunos; i++){
+    let aleatoria =  Math.floor(Math.random() * 100)
+    let situacao
+    if(aleatoria < 60){
+      situacao = 'reprovado'
+    } else {
+      situacao = 'aprovado'
+      aprovados++
+    }
+    let aluno = {nr: i % 5, nota: aleatoria, situacao: situacao}
+    alunos.push(aluno)
+    console.log(`Aluno ${aluno.nr} Nota ${aluno.nota} [${aluno.situacao.toUpperCase()}]`)
+  }
 
+  let reprovados = qtd_alunos - aprovados
+  let p_aprovado = (aprovados / qtd_alunos) * 100
+  let p_reprovado = (reprovados / qtd_alunos) * 100
+
+  console.log(`APROVADOS: ${aprovados} (${p_aprovado.toFixed()}%) | REPROVADOS: ${reprovados} (${p_reprovado.toFixed()}%)`)
+
+
+  // for calcular reprovacao
+
+  // relatorio
 
   // para cada aluno --->   Nota yy [(A/RE)PROVADO]"
   // no final "APROVADOS: XX (xx%) | REPROVADOS: YY (yy%)"
@@ -30,8 +61,15 @@ function questao4() {
   let numeros = []
   let tentativas = 0;
 
-  // loop
+  while(numeros.length < 1000){
+    tentativas++;
+    let aleatorio = Math.floor(Math.random() * 1000)
 
+    if(numeros.includes(aleatorio) === false){
+      numeros.push(aleatorio)
+    }
+  }
+  numeros.sort((x, y ) => x - y )
   console.log(tentativas)
 }
 
@@ -39,7 +77,13 @@ function questao4() {
 function questao5() {
 
   function tipoTriangulo(a, b, c) {
-    return ''
+    if(a === b && b === c){
+      return 'equilatero'
+    }
+    if(a !== b && b !== c && c !== a){
+      return 'escaleno'
+    }
+    return 'isoceles'
   }
 
   console.log(tipoTriangulo(5, 5, 5))
