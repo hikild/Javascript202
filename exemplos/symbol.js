5ff53dfebf24ea16942826ef0b19fa7b967504ba
Ezequiel Bertti <ebertti@gmail.com>
2020-05-27 21:51:56 -0300

    navegacao e dataset

--- exemplos/symbol.js
+++ exemplos/symbol.js
@@ -5,11 +5,13 @@ let notas_scretas = {}
 for (let i = 0; i < 10; i ++){
 
   let nota = Math.floor(Math.random() * 100)
-  let ns = Symbol()
+  let ns = Symbol('chave')
   notas_scretas[ns] = nota
 
   alunos.push({aluno: i, nota: ns})
 }
+
+print(alunos) // nao deve exibir as notas
 
 // volta da nota
 notas_scretas[alunos[0].nota]
