54a9fdb862ecec58bf684762095fabe595789ffa
Ezequiel Bertti <ebertti@gmail.com>
2020-05-29 20:28:41 -0300

    mais exemplo de symbol

--- exemplos/symbol.js
+++ exemplos/symbol.js
@@ -16,6 +16,25 @@ print(alunos) // nao deve exibir as notas
 // volta da nota
 notas_scretas[alunos[0].nota]
 
+let mapa = {}
+let colecao = []
+for (let i = 0; i < 20; i++) {
 
+  let obj = {}
+
+  for (let j = 0; j < 3; j++) {
+    let num = Math.ceil(Math.random() * 1000)
+    let ref = Symbol()
+    mapa[ref] = num
+
+    obj['a' + j] = ref
+  }
+
+  colecao.push(obj)
+}
+console.log(colecao)
+
+let posicao = 10
+console.log(mapa[colecao[posicao].a1])
 
 
